db222992b016f3c5cf53c198bcdbbdc02dba9559
Víctor Eduardo Orozco Martinez <victor_orozco_@outlook.com>
2024-09-04 10:52:41 -0700

    feat(view-component): Add LoginView
    this This view is missing the text of the app name

---
+++ client/src/components/views/Login.tsx
@@ -0,0 +1,9 @@
+import Login from "../Login";
+
+export default function LoginView() {
+  return (
+    <div className='backgrundpg bg-[#BCF8CB] h-screen w-screen grid justify-items-center items-center'>
+      <Login />
+    </div>
+  );
+}
