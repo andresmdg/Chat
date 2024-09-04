c653c11d71a258adbe27a5ca9c426a55ef1fec1a
Víctor Eduardo Orozco Martinez <victor_orozco_@outlook.com>
2024-09-04 10:49:26 -0700

    feat(view-component): Add HomeView component

---
+++ client/src/components/views/Home.tsx
@@ -0,0 +1,9 @@
+import SideBar from "../SideBar";
+
+export default function HomeView() {
+  return (
+    <div className='backgrundpg bg-[#BCF8CB] h-screen w-screen'>
+      <SideBar />
+    </div>
+  );
+}
