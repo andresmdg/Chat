caaeff6f815af356dac1d00e52faa8aab472b5e9
Víctor Eduardo Orozco Martinez <victor_orozco_@outlook.com>
2024-09-04 10:39:27 -0700

    feat(component): Add chatCard component

--- client/src/components/ChatCard.tsx
+++ client/src/components/ChatCard.tsx
@@ -3,26 +3,28 @@ export default function ChatCard({
   message,
   msnAccount,
   date,
+  image,
 }: {
   name: string;
   message: string;
   msnAccount: string;
   date: string;
+  image: string;
 }) {
   return (
     <div className='bg-slate-700 flex gap-2 p-2'>
       <img
-        src=''
+        src={`${image}`}
         alt='icon'
-        className='rounded-full h-11 w-11 border border-green-500 my-auto'
+        className='rounded-full h-11 w-11 border border-green-500 my-auto bg-fixed'
       />
-      <div className='grid justify-items-start gap-2'>
+      <div className='grid justify-items-start gap-2 w-full'>
         <h5 className='text-violet-500'>{name}</h5>
         <p className='text-green-600'>{message}</p>
       </div>
       <div className='grid justify-items-end gap-2'>
         <p>{date}</p>
-        <p className='px-2 bg-red-400 rounded-full'>
+        <p className='px-2 bg-red-400 rounded-full text-white'>
           {Number(msnAccount) <= 99 ? msnAccount : "99+"}
         </p>
       </div>
