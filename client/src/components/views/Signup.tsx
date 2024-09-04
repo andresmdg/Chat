c2fb8b66273df56de7a5d2e90b8211b214c292b4
Víctor Eduardo Orozco Martinez <victor_orozco_@outlook.com>
2024-09-04 10:53:17 -0700

    fix(view-component): add Signupview
    This view is missing the text of the app name

---
+++ client/src/components/views/Signup.tsx
@@ -0,0 +1,9 @@
+import Signup from "../Signup";
+
+export default function SignupView() {
+  return (
+    <div className='backgrundpg bg-[#BCF8CB] h-screen w-screen grid justify-items-center items-center'>
+      <Signup />/
+    </div>
+  );
+}
