0718375e7ef4bfdfc9f730700a28e0aa6b04d0f8
Víctor Eduardo Orozco Martinez <victor_orozco_@outlook.com>
2024-09-04 18:08:40 -0700

    feat(root): Add the use effecto to get the token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,21 @@ import "./App.css";
 import LoginView from "./components/views/Login";
 import SignupView from "./components/views/Signup";
 import HomeView from "./components/views/Home";
+import { useEffect } from "react";
 
 function App() {
+
+   useEffect(() => {
+    // Check if user is logged in
+    const token = localStorage.getItem("token");
+    if (token) {
+      // If user is logged in, redirect to home page
+      window.location.href = "/home";
+     }
+     
+    }, []);
+
+
   return (
     <Router>
       <Routes>
