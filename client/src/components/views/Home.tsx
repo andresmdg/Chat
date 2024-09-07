d2bbaf91a342b54c61ef28ef8b1027ffe6e5126b
Carlos Florez <carlos.andres.ff2912@gmail.com>
2024-09-06 20:29:11 -0500

    fix(Home.tsx): Add grid for layout

--- client/src/components/views/Home.tsx
+++ client/src/components/views/Home.tsx
@@ -2,7 +2,7 @@ import SideBar from "../sideBar/SideBar";
 
 export default function HomeView() {
   return (
-    <div className='backgrundpg bg-light-green h-screen w-screen'>
+    <div className='backgrundpg bg-light-green h-screen w-screen grid grid-cols-[auto_1fr]'>
       <SideBar />
     </div>
   );
