1cb0fc9cdd983bcbe6ec760f327a70bf4f2e7949
Carlos Florez <carlos.andres.ff2912@gmail.com>
2024-09-06 20:19:29 -0500

    feat(themeContext.tsx): Add Theme context

---
+++ client/src/context/themeContext.tsx
@@ -0,0 +1,41 @@
+import {createContext, useEffect, useState} from "react";
+import { LocalStorage } from "../utils/localStorage";
+
+export interface ThemeContetxValue {
+  white: boolean
+  changeTheme: () => void
+}
+
+const initialValue: ThemeContetxValue = {
+  white: false,
+  changeTheme: (): void => { throw new Error("Function not implemented.") },
+}
+
+export const ThemeContext = createContext<ThemeContetxValue>(initialValue);
+
+
+export const ThemeContextProvider = ({ children }: {children: React.ReactNode}) => {
+  const [white, setWhite] = useState(false);
+
+  const changeTheme = () => {
+    setWhite(!white);
+    LocalStorage.setItem("white", white)
+  }
+
+  useEffect(() => {
+    const storage = LocalStorage.getItem("white");
+    if (storage) {
+      setWhite(storage);
+    }
+  }, []);
+
+  const value = {
+    white,
+    changeTheme
+  }
+  return (
+    <ThemeContext.Provider value={value as ThemeContetxValue}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
