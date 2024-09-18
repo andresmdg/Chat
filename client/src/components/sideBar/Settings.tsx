4daf54c438dd93d2068a61c9a86b4bf15ab69f48
Víctor Eduardo Orozco Martinez <victor_orozco_@outlook.com>
2024-09-17 18:23:45 -0700

    style(component-settings-in-sidebar): add media querys

--- client/src/components/sideBar/Settings.tsx
+++ client/src/components/sideBar/Settings.tsx
@@ -12,21 +12,38 @@ export default function Settings({
   settings: boolean;
   setSettings: Dispatch<SetStateAction<boolean>>;
 }) {
-  const {white, changeTheme} = useTheme();
+  const { white, changeTheme } = useTheme();
 
   return (
-    <div className='grid sidebar_Row w-96 gap-4 text-start p-4'>
-      <InfoAccount
-        white={white}
-        name='Name'
-        email='user@mail.com'
-        userName='User'
-      />
+    <div
+      className='grid sidebar_Row w-96 gap-4 text-start p-4 h-full
+    max-md:w-screen
+      max-lg:w-fit
+      max-2xl:w-80
+      
+    '>
+      <div className='max-sm:flex max-sm:justify-between max-sm:items-center max-sm:gap-4'>
+        <button
+          onClick={() => handleActive(settings, setSettings)}
+          className={`h-full w-full rounded-lg p-4 
+          hidden max-sm:block
+          ${white ? "bg-[#ffffff]" : "bg-yellow-50"}`}>
+          <IoIosArrowBack className='h-6 w-6' />
+        </button>
+        <InfoAccount
+          white={white}
+          name='Name'
+          email='user@mail.com'
+          userName='User'
+        />
+      </div>
+      {/* // * Back button */}
       <button
         onClick={() => handleActive(settings, setSettings)}
-        className={`h-12 w-6 rounded-s p-0 absolute top-20 left-80 ${
-          white ? "bg-[#ffffff]" : "bg-yellow-50"
-        }`}>
+        className={`h-12 w-6 rounded-s p-0 absolute top-20 left-96 
+          max-sm:hidden
+          max-2xl:left-80
+          ${white ? "bg-[#ffffff]" : "bg-yellow-50"}`}>
         <IoIosArrowBack />
       </button>
       <div className='grid sidebar_Button h-full content-between gap-4'>
@@ -58,9 +75,7 @@ export default function Settings({
           } p-2 rounded-lg text-center`}>
           <p>Temas</p>
           <div className='flex gap-2'>
-            <button
-              onClick={changeTheme}
-              className='p-2'>
+            <button onClick={changeTheme} className='p-2'>
               <div className='rounded-full h-6 w-6 bg-white border border-black' />
             </button>
           </div>
