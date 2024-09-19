742e8f5311a8c1b784e7e3d40c0a91326c8a668b
andresmdg <andresmdg99@gmail.com>
2024-09-18 22:14:29 -0600

    fix(initdb): comma added

--- server/src/utils/initdb.js
+++ server/src/utils/initdb.js
@@ -19,7 +19,7 @@ async function initDatabase(db) {
           username VARCHAR UNIQUE,
           password VARCHAR NOT NULL,
           avatar	VARCHAR,
-          online BOOLEAN DEFAULT FALSE
+          online BOOLEAN DEFAULT FALSE,
           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
       `,
