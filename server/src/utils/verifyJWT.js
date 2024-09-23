cdf77d3fba3989aad87015ece38b38272ebb7d7e
andresmdg <andresmdg99@gmail.com>
2024-09-23 05:56:13 -0600

    fix(*): undefined scrt key

--- server/src/utils/verifyJWT.js
+++ server/src/utils/verifyJWT.js
@@ -1,13 +1,21 @@
 import jwt from "jsonwebtoken";
+import { Log } from "#utils";
 
-const { SCRT } = process.env;
-
-const verifyJWT = (token = "") => {
+const verifyJWT = async (token = "") => {
   try {
+    const { SCRT } = process.env;
     const { id } = jwt.verify(token, SCRT);
-
     return [true, id];
   } catch (error) {
+    const errorMsg =
+      error.name === "JsonWebTokenError"
+        ? "Invalid token"
+        : "Token verification error";
+    new Log(
+      `JWT verification failed  |  [REASON] ${errorMsg}: ${error.message}`,
+      "error",
+      "auth"
+    );
     return [false, null];
   }
 };
