25ce1053ece26cdb6fdb58a4b8fb98d692134570
Carlos Florez <carlos.andres.ff2912@gmail.com>
2024-09-23 09:26:33 -0500

    feat(relogin.ts): Add relogin service

--- client/src/services/auth/index.ts
+++ client/src/services/auth/index.ts
@@ -1,9 +1,11 @@
 import { AccessToken } from './accessToken'
 import { login } from './login'
+import { relogin } from './relogin'
 import { signup } from './signup'
 
 export const Auth = {
   login,
   signup,
+  relogin,
   AccessToken,
 }
