eda7b39e964779b0be0265af98873240af7f0b7e
andresmdg <andresmdg99@gmail.com>
2024-09-24 01:19:35 -0600

    fix(app.js): format style

--- server/src/app.js
+++ server/src/app.js
@@ -18,78 +18,75 @@ const server = createServer(app)
 const io = new Server(server)
 
 const eventsDir = path.join(__dirname, 'src/events')
-const routesDir = path.join(
-  __dirname,
-  'src/routes'
-)(
-  // Main function
+const routesDir = path.join(__dirname, 'src/routes')
+// Main function
 
-  async () => {
-    try {
-      // DB connection
-      await db()
-      // loading routers
-      await loadRouters(app, routesDir)
+;(async () => {
+  // No borrar el ;
+  try {
+    // DB connection
+    await db()
+    // loading routers
+    await loadRouters(app, routesDir)
 
-      // Running server
-      const listener = server.listen(
-        PORT,
-        () => new Log(`Listening on port: ${listener.address().port}`, 'info')
-      )
+    // Running server
+    const listener = server.listen(
+      PORT,
+      () => new Log(`Listening on port: ${listener.address().port}`, 'info')
+    )
 
-      // Websocket listener
-      io.on('connection', async socket => {
-        new Log('Connection with a user established', 'info', 'socket')
+    // Websocket listener
+    io.on('connection', async socket => {
+      new Log('Connection with a user established', 'info', 'socket')
 
-        // Token Validation
-        const [valid, uuid] = await verifyJWT(socket.handshake.query['x-token'])
-        if (!valid) {
-          new Log(
-            'Client disconnected  |  [REASON]  Invalid session token',
-            'warning',
-            'socket'
-          )
-          return socket.disconnect()
-        }
+      // Token Validation
+      const [valid, uuid] = await verifyJWT(socket.handshake.query['x-token'])
+      if (!valid) {
+        new Log(
+          'Client disconnected  |  [REASON]  Invalid session token',
+          'warning',
+          'socket'
+        )
+        return socket.disconnect()
+      }
 
-        try {
-          await usersModel.userOnline(uuid)
+      try {
+        await usersModel.userOnline(uuid)
 
-          const chatIds = await chatsModel.getUserChats(uuid)
+        const chatIds = await chatsModel.getUserChats(uuid)
 
-          if (chatIds) {
-            new Log('Chats id ' + chatIds, 'info', 'socket')
-            chatIds.forEach(chatId => {
-              new Log(chatId, 'info', 'socket')
-              socket.join(chatId)
-            })
-          }
-
-          // Load socket events
-          await loadEvents(uuid, socket, io, eventsDir)
-
-          // Client emit events
-          socket.onAny((eventName, ...args) => {
-            new Log(
-              `Event emitted by client  |  [EVENT]  '${eventName}'`,
-              'info',
-              'socket'
-            )
+        if (chatIds) {
+          new Log('Chats id ' + chatIds, 'info', 'socket')
+          chatIds.forEach(chatId => {
+            new Log(chatId, 'info', 'socket')
+            socket.join(chatId)
           })
-        } catch (error) {
+        }
+
+        // Load socket events
+        await loadEvents(uuid, socket, io, eventsDir)
+
+        // Client emit events
+        socket.onAny((eventName, ...args) => {
           new Log(
-            `Error joining chat rooms  |  [MESSAGE]  ${error.message}`,
-            'error',
+            `Event emitted by client  |  [EVENT]  '${eventName}'`,
+            'info',
             'socket'
           )
-          socket.disconnect()
-        }
-      })
-    } catch (error) {
-      new Log(error.message, error.level, error.emisor)
-      setTimeout(() => {
-        process.exit(1)
-      }, 1000)
-    }
+        })
+      } catch (error) {
+        new Log(
+          `Error joining chat rooms  |  [MESSAGE]  ${error.message}`,
+          'error',
+          'socket'
+        )
+        socket.disconnect()
+      }
+    })
+  } catch (error) {
+    new Log(error.message, error.level, error.emisor)
+    setTimeout(() => {
+      process.exit(1)
+    }, 1000)
   }
-)()
+})()
